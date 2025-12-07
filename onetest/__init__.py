e2a90ff1f9271f0c981cbb526b38910c11d22d77
chivehao <chivehao@ikaros.run>
2025-12-07 20:06:40 +0800

    permission

--- onetest/__init__.py
+++ onetest/__init__.py
@@ -1,8 +1,15 @@
-from mcdreforged import PluginServerInterface
-
-from onetest import my_lib
+from mcdreforged import Info, PluginServerInterface
 
 
 def on_load(server: PluginServerInterface, old):
     server.logger.info(server.tr('my_plugin.a_message'))
-    my_lib.do_something()
+
+def on_user_info(server: PluginServerInterface, info: Info):
+    server.reply(info, "{} say {}".format(info.player, info.content))
+    permission = server.get_permission_level(info)
+    server.reply(info, "{} permission level is {}".format(info.player, permission))
+    if (server.get_permission_level(info) == 4) and info.content == 'restart':
+        server.restart()
+    if (server.get_permission_level(info) == 4):
+        server.broadcast('broadcast msg')
+
