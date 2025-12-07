67673144caae4f842f1c300cdd08acfac86d006a
chivehao <chivehao@ikaros.run>
2025-12-07 16:01:56 +0800

    Initial commit

---
+++ my_plugin/__init__.py
@@ -0,0 +1,8 @@
+from mcdreforged import PluginServerInterface
+
+from my_plugin import my_lib
+
+
+def on_load(server: PluginServerInterface, old):
+    server.logger.info(server.tr('my_plugin.a_message'))
+    my_lib.do_something()
