c5aa360bd34d26b967e0b78ebbcf7ab3ae725133
chivehao <chivehao@ikaros.run>
2025-12-09 05:27:00 +0800

    添加玩家在线列表接口

--- onetest/__init__.py
+++ onetest/__init__.py
@@ -19,28 +19,55 @@ server_status:ServerStatus = ServerStatus.DOWN
 def on_server_start_pre(server: PluginServerInterface):
     global server_status
     server_status = ServerStatus.START_PRE
-    server.logger.info('server start pre.')
+    server.logger.info('update server status to {}'.format(server_status.name))
 
 def on_server_start(server: PluginServerInterface):
     global server_status
     server_status = ServerStatus.STARTING
-    server.logger.info('server is starting.')
+    server.logger.info('update server status to {}'.format(server_status.name))
 
 def on_server_startup(server: PluginServerInterface):
     global server_status
     server_status = ServerStatus.UP
-    server.logger.info('server has started.')
+    server.logger.info('update server status to {}'.format(server_status.name))
+    global players
+    players = []
 
 def on_server_stop(server: PluginServerInterface, server_return_code: int):
     # if server_return_code != 0:
     #     server.logger.info('Is it a server crash?')
-    server.logger.info('server stopped.')
     global server_status
     server_status = ServerStatus.DOWN
+    server.logger.info('update server status to {}'.format(server_status.name))
 
 @app.get("/server/status")
 async def get_server_status():
     return server_status.name
+
+players = []
+
+def on_player_joined(server: PluginServerInterface, player: str, info: Info):
+    server.say('Welcome {}'.format(player))
+    global players
+    if player not in players:
+        players.append(player)
+        server.logger.info('add {} to players'.format(player))
+
+def on_player_left(server: PluginServerInterface, player: str):
+    global players
+    if player in players:
+        players.remove(player)
+        server.logger.info('remove {} from players'.format(player))
+
+
+@app.get("/server/players")
+async def get_server_players():
+    global players
+    return players
+    if server_status == ServerStatus.UP:
+        return players
+    else:
+        return server_status.name
 
 def mount_app(server):
     # save plugin id and fastapi_mcdr instance
