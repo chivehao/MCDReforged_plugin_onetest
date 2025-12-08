7e6aef639ec0ccfa4939a64dc312a2d1a083857c
chivehao <chivehao@ikaros.run>
2025-12-09 04:46:24 +0800

    添加服务状态查询接口

--- onetest/__init__.py
+++ onetest/__init__.py
@@ -1,11 +1,46 @@
 from mcdreforged import Info, PluginServerInterface
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 @app.get("/test")
 async def test():
     return "Hello, world!"
+
+class ServerStatus(Enum):
+    DOWN = 1
+    UP = 2
+    START_PRE = 3
+    STARTING = 4
+    
+server_status:ServerStatus = ServerStatus.DOWN
+
+def on_server_start_pre(server: PluginServerInterface):
+    global server_status
+    server_status = ServerStatus.START_PRE
+    server.logger.info('server start pre.')
+
+def on_server_start(server: PluginServerInterface):
+    global server_status
+    server_status = ServerStatus.STARTING
+    server.logger.info('server is starting.')
+
+def on_server_startup(server: PluginServerInterface):
+    global server_status
+    server_status = ServerStatus.UP
+    server.logger.info('server has started.')
+
+def on_server_stop(server: PluginServerInterface, server_return_code: int):
+    # if server_return_code != 0:
+    #     server.logger.info('Is it a server crash?')
+    server.logger.info('server stopped.')
+    global server_status
+    server_status = ServerStatus.DOWN
+
+@app.get("/server/status")
+async def get_server_status():
+    return server_status.name
 
 def mount_app(server):
     # save plugin id and fastapi_mcdr instance
@@ -36,12 +71,12 @@ def on_unload(server):
     # unmount app
     fastapi_mcdr.unmount(id_)
 
-def on_user_info(server: PluginServerInterface, info: Info):
-    server.reply(info, "{} say {}".format(info.player, info.content))
-    permission = server.get_permission_level(info)
-    server.reply(info, "{} permission level is {}".format(info.player, permission))
-    if (server.get_permission_level(info) == 4) and info.content == 'restart':
-        server.restart()
-    if (server.get_permission_level(info) == 4):
-        server.broadcast('broadcast msg')
+# def on_user_info(server: PluginServerInterface, info: Info):
+#     server.reply(info, "{} say {}".format(info.player, info.content))
+#     permission = server.get_permission_level(info)
+#     server.reply(info, "{} permission level is {}".format(info.player, permission))
+#     if (server.get_permission_level(info) == 4) and info.content == 'restart':
+#         server.restart()
+#     if (server.get_permission_level(info) == 4):
+#         server.broadcast('broadcast msg')
 
